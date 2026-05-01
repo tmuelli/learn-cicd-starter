9c85c71c2660921d6689279c4406b8122c189479
tmuelli <tm.muellertom@gmail.com>
2026-05-01 15:11:41 +0200

    FIX: fixed bug for temporary workflow fails

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,8 +10,6 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	// temporary to fail ci workflow tests
-	return "not my header"
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
